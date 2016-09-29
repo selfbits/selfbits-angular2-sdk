3102ee7343eff30ac5a3f86d8cd5edbeae9f4d7f
Han Che <han.che@selfbits.org>
2016-09-29 11:46:15 +0200

    specified what to export in index and bumped version

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-export * from './src/angularselfbits'
+// export * from './src/angularselfbits'
+
+export { SelfbitsAngular, SelfbitsAngularModule, SELFBITS_PROVIDERS } from './src/angularselfbits'
+export { SelfbitsAppConfig, SelfbitsAuthConfig, SelfbitsHttp } from './src/utils/interfaces'
+export { SELFBITS_CONFIG } from './src/utils/tokens'
